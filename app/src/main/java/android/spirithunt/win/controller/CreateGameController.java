631bd7d01ce3ef85e283e82d65ddf21b19e1f437
Remco Schipper <github@remcoschipper.com>
2017-04-11 14:24:57 +0200

    Moved the comment

---
+++ app/src/main/java/android/spirithunt/win/controller/CreateGameController.java
@@ -0,0 +1,17 @@
+package android.spirithunt.win.controller;
+
+import android.os.Bundle;
+import android.spirithunt.win.R;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author Remco Schipper
+ */
+
+public class CreateGameController extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_game_view);
+    }
+}
--- app/src/main/java/android/spirithunt/win/controller/MenuController.java
+++ app/src/main/java/android/spirithunt/win/controller/MenuController.java
@@ -1,6 +1,10 @@
 package android.spirithunt.win.controller;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.spirithunt.win.R;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by sven on 30-3-17.
@@ -8,5 +12,14 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class MenuController extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_view);
+    }
 
+    public void createGame(View view) {
+        Intent intent = new Intent(this, CreateGameController.class);
+        startActivity(intent);
+    }
 }
