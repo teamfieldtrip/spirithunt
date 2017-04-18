c1de647dde34f6c215733bfd8768baae32357b88
Roelof Roos <github@roelof.io>
2017-04-18 14:10:00 +0200

    Added android.spirithunt.wn.model.Location class

---
+++ app/src/main/java/android/spirithunt/win/model/Location.java
@@ -0,0 +1,35 @@
+package android.spirithunt.win.model;
+
+/**
+ * Created by roelof on 13/04/17.
+ */
+
+public class Location extends android.location.Location {
+    /**
+     * Builds a Location object from a given latitude and longitude provided by the named provider
+     * @param latitude
+     * @param longitude
+     * @param provider
+     * @return
+     */
+    public static Location build(double latitude, double longitude, String provider) {
+        Location res = new Location(provider);
+        res.setLatitude(latitude);
+        res.setLongitude(longitude);
+        return res;
+    }
+
+    /**
+     * Builds a Location object from a given latitude and longitude. The provider is "user".
+     * @param latitude
+     * @param longitude
+     * @return
+     */
+    public static Location build(double latitude, double longitude) {
+        return build(latitude, longitude, "user");
+    }
+
+    public Location(String provider) {
+        super(provider);
+    }
+}
