db2889e1ddd2b13b21debde0c265ef95c228ec69
Roelof Roos <github@roelof.io>
2017-05-12 15:43:35 +0200

    Added DialogProvider

---
+++ app/src/main/java/win/spirithunt/android/provider/DialogProvider.java
@@ -0,0 +1,60 @@
+package win.spirithunt.android.provider;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+
+import win.spirithunt.android.R;
+
+/**
+ * Creates dialogs, such as progress dialgs.
+ *
+ * @author Roelof Roos
+ */
+
+public class DialogProvider {
+
+    private ProgressDialog progressDialog;
+
+    private Activity activity;
+
+    public DialogProvider(Activity activity) {
+        this.activity = activity;
+    }
+
+    /**
+     * Returns if a dialog is visible.
+     * @return
+     */
+    public boolean isProgressDialogOpen() {
+        return progressDialog != null;
+    }
+
+    /**
+     * Closes an open progress dialog, if any.
+     */
+    public void hideProgressDialog() {
+        if(progressDialog != null) {
+            progressDialog.dismiss();
+            progressDialog = null;
+        }
+    }
+
+    /**
+     * Opens a progress dialog, specify the title and body. The progress is non-cancellable.
+     *
+     * @param title Title of the dialog
+     * @param body Body of the dialog
+     */
+    public void showProgressDialog(String title, String body) {
+        if(progressDialog == null) {
+            progressDialog = new ProgressDialog(activity, R.style.AppDialog);
+            progressDialog.setTitle(title);
+            progressDialog.setMessage(body);
+            progressDialog.setIndeterminate(true);
+            progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+        }
+    }
+}
+
