6cf49c83a8a77837d3c4d394c898b8f2bc5751eb
Roelof Roos <github@roelof.io>
2017-05-16 00:19:40 +0200

    Added startGame support, which isn't used yet

--- app/src/main/java/win/spirithunt/android/controller/LobbyController.java
+++ app/src/main/java/win/spirithunt/android/controller/LobbyController.java
@@ -155,11 +155,13 @@ public class LobbyController extends AppCompatActivity {
                     provider.hideProgressDialog();
                 }
 
-                /*
-                    TODO process response, it's a single object containing id, game, players and target
-                     but how we're sending this and what the game looks like... It's TBD
-                 */
-                // TODO Add data
+                Player[] ply = (Player[]) args[0];
+                gameIntent.putExtra("players", ply);
+                gameIntent.putExtra("name", (String) args[1]);
+                gameIntent.putExtra("centerLat", (double) args[2]);
+                gameIntent.putExtra("centerLng", (double) args[3]);
+                gameIntent.putExtra("radius", (double) args[4]);
+                gameIntent.putExtra("target", (Player) args[5]);
                 self.startActivity(gameIntent);
             }
         });
