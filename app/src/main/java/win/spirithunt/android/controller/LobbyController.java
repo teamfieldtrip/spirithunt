6e57446f094cbb1c0c116cc6ffc027ace594401d
Roelof Roos <github@roelof.io>
2017-05-16 00:27:29 +0200

    Added startGame and TODO

--- app/src/main/java/win/spirithunt/android/controller/LobbyController.java
+++ app/src/main/java/win/spirithunt/android/controller/LobbyController.java
@@ -93,6 +93,21 @@ public class LobbyController extends AppCompatActivity {
                 });
             }
         });
+
+        // TODO player abandonment
+
+        socket.on("lobby:started", new Emitter.Listener() {
+            @Override
+            public void call(final Object... args) {
+                self.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String gameId = (String) args[0];
+                        self.startGame(gameId);
+                    }
+                });
+            }
+        });
     }
 
     @Override
@@ -126,13 +141,11 @@ public class LobbyController extends AppCompatActivity {
                     provider.hideProgressDialog();
                 }
 
-                Player[] ply = (Player[]) args[0];
-                gameIntent.putExtra("players", ply);
-                gameIntent.putExtra("name", (String) args[1]);
-                gameIntent.putExtra("centerLat", (double) args[2]);
-                gameIntent.putExtra("centerLng", (double) args[3]);
-                gameIntent.putExtra("radius", (double) args[4]);
-                gameIntent.putExtra("target", (Player) args[5]);
+                /*
+                    TODO process response, it's a single object containing id, game, players and target
+                     but how we're sending this and what the game looks like... It's TBD
+                 */
+                // TODO Add data
                 self.startActivity(gameIntent);
             }
         });
