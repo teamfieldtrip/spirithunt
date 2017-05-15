929c2203a16bcd26f309731e80bf95f3dbcb22f1
Roelof Roos <github@roelof.io>
2017-05-16 00:16:07 +0200

    Moved tagging

--- app/src/main/java/win/spirithunt/android/controller/GameController.java
+++ app/src/main/java/win/spirithunt/android/controller/GameController.java
@@ -108,16 +108,7 @@ public class GameController extends AppCompatActivity implements View.OnClickLis
 //                usePowerup(view);
 //                break;
             case R.id.game_tag:
-                // TODO hold button for 3 seconds to tag person (probably use onLongClickListener)
-                Log.d(TAG, "onClick: Person has been tagged");
-                Button btnTag = (Button) findViewById(R.id.game_tag);
-                btnTag.setVisibility(View.INVISIBLE);
-
-                Socket socket = SocketProvider.getInstance().getConnection();
-                final GameController self = this;
-
-                socket.emit("gameplay:tag", new GameTag(ownPlayer.getId(), target.getId()));
-                // TODO get confirmation
+                tagPlayer();
                 break;
             default:
                 break;
@@ -190,7 +181,16 @@ public class GameController extends AppCompatActivity implements View.OnClickLis
      * meantime.
      */
     private void tagPlayer() {
+        Log.d(TAG, "onClick: Person has been tagged");
+        Button btnTag = (Button) findViewById(R.id.game_tag);
+        btnTag.setVisibility(View.INVISIBLE);
 
+        Socket socket = SocketProvider.getInstance().getConnection();
+        final GameController self = this;
+
+        socket.emit("gameplay:tag", new GameTag(ownPlayer.getId(), target.getId()));
+        // TODO get confirmation
+        // TODO hold button for 3 seconds to tag person
     }
 }
 
